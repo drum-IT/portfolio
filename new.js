53b48d056e30baf7d5d24550f79024208d7acc6f
Miles Pratt <miles@Slice.local>
2018-12-03 11:32:48 -0500

    Create new portfolio layout.

---
+++ new.js
@@ -0,0 +1,11 @@
+const projectLinks = document.querySelectorAll(".project__link");
+// setTimeout(() => {
+//   projectLinks.forEach(link => {
+//     const parentWidth = link.parentNode.clientWidth;
+//     const parentHeight = link.parentNode.clientHeight;
+//     const linkWidth = link.clientWidth;
+//     const linkHeight = link.clientHeight;
+//     link.style.marginLeft = `${parentWidth / 2 - linkWidth / 2}px`;
+//     link.style.marginTop = `${parentHeight / 2 - linkHeight / 2}px`;
+//   });
+// }, 2000);
